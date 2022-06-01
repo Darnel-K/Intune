2257ce6492c11210b55ecbe7a0d59073573f9ae0
Darnel Kumar <darnel.kumar@dgk.one>
2022-06-01 13:21:03 +0100

    Modified verbose messages script

--- Scripts/Turn Verbose Status Messages On.ps1
+++ Scripts/Turn Verbose Status Messages On.ps1
@@ -1,2 +1,39 @@
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "verbosestatus" -Value "1" -Type DWORD
-Set-Itemproperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'DisableStatusMessages' -value '0'
+$RegKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$VerboseProperty = "verbosestatus"
+$StatusMsgProperty = "DisableStatusMessages"
+
+if ((Get-ItemProperty $RegKey).PSObject.Properties.Name -contains $VerboseProperty) {
+    try {
+        Set-Itemproperty -path $RegKey -Name $VerboseProperty -value '1'
+    }
+    catch {
+        $_.Exception.Message
+    }
+}
+else {
+    try {
+        New-ItemProperty -path $RegKey -Name $VerboseProperty -value '1' -Type DWORD
+    }
+    catch {
+        $_.Exception.Message
+    }
+
+}
+if ((Get-ItemProperty $RegKey).PSObject.Properties.Name -contains $StatusMsgProperty) {
+    try {
+        Set-Itemproperty -path $RegKey -Name $StatusMsgProperty -value '0'
+    }
+    catch {
+        $_.Exception.Message
+    }
+}
+else {
+    try {
+        New-ItemProperty -path $RegKey -Name $StatusMsgProperty -value '0' -Type DWORD
+    }
+    catch {
+        $_.Exception.Message
+    }
+
+}
+Exit $LASTEXITCODE
