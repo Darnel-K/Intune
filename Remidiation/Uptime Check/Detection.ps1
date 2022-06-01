18a52911a65eab053092c24464f2c7e7250280df
Darnel Kumar <darnel.kumar@dgk.one>
2022-06-01 22:29:07 +0100

    Removed uptime check
    Not created by me, author unknown

--- Remidiation/Uptime Check/Detection.ps1
+++
@@ -1,7 +0,0 @@
-$Uptime= get-computerinfo | Select-Object OSUptime 
-if ($Uptime.OsUptime.Days -ge 7){
-    Write-Output "Device has not rebooted in $($Uptime.OsUptime.Days) days, notify user to reboot"
-    Exit 1
-}else {
-    Write-Output "Device has rebooted $($Uptime.OsUptime.Days) days ago, all good"
-    Exit 0
