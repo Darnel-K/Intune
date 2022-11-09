4867574c3355a234d9d58474d6c9ad09a314de90
Darnel Kumar <darnel.kumar@dgk.one>
2022-11-09 10:16:06 +0000

    Moved to Public Repo

--- Scripts/Get list of users not in group.ps1
+++
@@ -1,5 +0,0 @@
-# Change GroupName to the name of the group
-# Run this on the local AD server
-
-$group = Get-ADGroup GroupName
-Get-ADUser -Properties memberof -Filter 'enabled -eq $true' | Where-Object { $group.DistinguishedName -notin $_.memberof } | Format-Table DistinguishedName
